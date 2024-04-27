12c941d5199b9436a45547d8402ee538a3d74998
pkepowicz <paw.kepowicz@gmail.com>
2024-04-27 08:09:21 +0200

    wedka w dobrym miejscu

--- Assets/Scripts/Rod/CircleMovement.cs
+++ Assets/Scripts/Rod/CircleMovement.cs
@@ -22,6 +22,31 @@ public class CircleeMovement : MonoBehaviour
     void Start()
     {
         currentAngle = endAngle;
+
+        updateRotation();
+    }
+
+    void updateRotation()
+    {
+
+        float x = Mathf.Cos(currentAngle * Mathf.Deg2Rad) * radius;
+        float y = Mathf.Sin(currentAngle * Mathf.Deg2Rad) * radius;
+
+
+        Vector2 endPosition = new Vector2(x, y) + (Vector2)center.transform.position;
+        Vector2 direction = endPosition - (Vector2)center.transform.position;
+
+        transform.position = endPosition - direction;
+
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+
+        if (objectToFollow != null)
+        {
+            objectToFollow.position = endPosition;
+
+        }
     }
 
     void Update()
@@ -29,43 +54,22 @@ public class CircleeMovement : MonoBehaviour
         if (fl.isFollowing && !Input.GetButton("Jump")) { fl.readyToCast = true; }
         if (fl.isFollowing == true && Input.GetButton("Jump") && fl.readyToCast)
         {
+            currentAngle -= angularSpeed * Time.deltaTime;
 
-                currentAngle -= angularSpeed * Time.deltaTime;
+            if (currentAngle < endAngle)
+            {
+                currentAngle = startAngle;
+            }
+            else if (currentAngle > startAngle)
+            {
+                currentAngle = endAngle;
+            }
 
-                if (currentAngle < endAngle)
-                {
-                    currentAngle = startAngle;
-                }
-                else if (currentAngle > startAngle)
-                {
-                    currentAngle = endAngle;
-                }
-
-
-                float x = Mathf.Cos(currentAngle * Mathf.Deg2Rad) * radius;
-                float y = Mathf.Sin(currentAngle * Mathf.Deg2Rad) * radius;
-
-
-                Vector2 endPosition = new Vector2(x, y) + (Vector2)center.transform.position;
-                Vector2 direction = endPosition - (Vector2)center.transform.position;
-
-                transform.position = endPosition - direction;
-
-
-                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-                transform.rotation = Quaternion.Euler(0, 0, angle);
-
-                if (objectToFollow != null)
-                {
-                    objectToFollow.position = endPosition;
-
-                }
-                return;
+            updateRotation();
+            return;
         }
-        else
-        {
-            currentAngle = endAngle;
-        }
+        currentAngle = 0;
+        updateRotation();
     }
 }
 
