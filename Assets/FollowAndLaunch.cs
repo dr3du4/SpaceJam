d9745ab8d46e55e05ce7fda2405a4928fc780aaa
Kamil Butryn <kaybenot@gmail.com>
2024-04-27 05:28:59 +0200

    Revert "Merge branch 'dr3d' of https://github.com/dr3du4/SpaceJam"
    This reverts commit a22483ba57dbfebeae7462a8f1cc3ab0b5eeb83f, reversing
    changes made to 07abf4972740f6d08b0cde56b34ac8d36a88b841.

---
+++ Assets/FollowAndLaunch.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
+
+public class FollowAndLaunch : MonoBehaviour
+{
+  
+    public Transform target;
+   
+
+    private Rigidbody2D rb;
+    public CircleeMovement rod;
+    
+    public float launchForce = 500f;
+    
+    public bool isFollowing = true;
+    public GameObject mainCamera;
+    public GameObject followingCamera;
+    void Start()
+    {
+        
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+        if (Input.GetKeyUp(KeyCode.Space) && isFollowing)
+        {
+            LaunchObject();
+            isFollowing = false;
+            
+        }
+
+     
+        if (isFollowing)
+        {
+            FollowTarget();
+        }
+
+        if (Vector3.Distance(transform.position,rod.center ) <= 0.1f)
+        {
+            isFollowing = true; 
+            
+        }
+    }
+
+   
+    void FollowTarget()
+    {
+        if (target != null)
+        {
+          
+            transform.position = target.position;
+        }
+    }
+
+ 
+    void LaunchObject()
+    {
+       
+        
+        Vector2 launchDirection = new Vector2(Mathf.Cos(Mathf.Deg2Rad * 45), Mathf.Sin(Mathf.Deg2Rad * 45));
+
+        rb.AddForce(launchDirection * launchForce, ForceMode2D.Impulse);
+        
+        
+        mainCamera.SetActive(false);
+        followingCamera.SetActive(true);
+    }
+}
